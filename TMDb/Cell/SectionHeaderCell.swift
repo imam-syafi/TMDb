0654bce3b73d1cfdf579ba2985ea2156ee363e46
imsyf <imsyf@imsyfs-Mac.local>
2023-03-14 15:38:36 +0700

    Display section header

---
+++ TMDb/Cell/SectionHeaderCell.swift
@@ -0,0 +1,20 @@
+//
+//  SectionHeaderCell.swift
+//  TMDb
+//
+//  Created by imsyf on 3/14/23.
+//  Copyright © 2023 EDTS. All rights reserved.
+//
+
+import UIKit
+
+class SectionHeaderCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
